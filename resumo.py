c1cce9bac0ba5d8958a4afd553485680752d0532
CosmoAndrade <cosmo.andrade@hotmail.com>
2020-08-02 14:03:29 -0300

    parte 03

--- resumo.py
+++ resumo.py
@@ -1,18 +1,24 @@
 numero_secreto = 42
 total_de_tentativas = 3
+total_de_tentativas = 3
+rodada = 1
 
 while (total_de_tentativas > 0):
+    print('Tentativa {} de {}'.format(rodada, total_de_tentativas))
     chute = int(input('Digite o seu número: '))
+
     print('Você digitou: ', chute)
 
-    acertou = chute == numero_secreto
+    acertou = numero_secreto == chute
     maior = chute > numero_secreto
     menor = chute < numero_secreto
-    if(acertou):
-        print('Você acertou')
-    elif(maior):
-        print('Você errou! O seu chute foi maior que o número secreto')
-    elif(menor):
-        print('Você errou! O seu chute foi menor que o número secreto')
 
-    total_de_tentativas = total_de_tentativas - 1
+    if (acertou):
+        print('Você acertou!')
+        break
+    elif (maior):
+        print('Você errou! O seu chute foi maior que o número secreto')
+    elif (menor):
+        print('Você errou! O seu chute foi menor que o número secreto')
+    rodada = rodada + 1
+print('Fim do jogo')
